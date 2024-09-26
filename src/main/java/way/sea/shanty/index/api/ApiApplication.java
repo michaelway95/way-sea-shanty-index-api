ef9db3c7e2cba3e9a927b6499fff95bc38ad5f36
michaelway95 <michael-way_@hotmail.com>
2024-09-26 21:58:15 +0100

    Project structure

---
+++ src/main/java/way/sea/shanty/index/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package way.sea.shanty.index.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/way/sea/shanty/index/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package way.sea.shanty.index.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
